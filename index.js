8fb7faf8e0185cf26418cb3b9bc8b51e26219387
edsonllima <megadev@DESKTOP-GCF2849.localdomain>
2022-07-07 10:38:14 -0300

    testando formas de acesso a classe por referencia

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+// const cliente2 = new Cliente();
+// cliente2.nome = "Alice";
+// cliente2.cpf = 88822233309;
 
 const Conta1 = new ContaCorrente();
 Conta1.agencia = 1001;
@@ -16,8 +16,11 @@ Conta1.cliente = cliente1;
 Conta1.depositar(500);
 
 const Conta2 = new ContaCorrente();
+Conta2.cliente = new Cliente();
+Conta2.cliente.nome = "Alice";
+Conta2.cliente.cpf = 88822233309;
 Conta2.agencia = 1001;
-Conta2.cliente = cliente2;
+
 Conta1.transferir(200, Conta2);
 
 console.log(Conta1);
